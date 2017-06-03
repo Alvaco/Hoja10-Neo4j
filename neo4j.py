4fd67ef202ec001bcc727984de13141cded5c814
Fernanda Estrada <est14198@uvg.edu.gt>
2017-06-02 21:38:55 -0600

    Add files via upload
    Ligeros cambios. Versión 1.2

--- neo4j.py
+++ neo4j.py
@@ -1,3 +1,16 @@
+# neo4j.py
+# Algoritmos y Estructura de Datos - Seccion 10
+# Christopher Sandoval 13660
+# Maria Fernanda Estrada 14198
+# Ana Lucia Diaz
+# Alejandro Vasquez
+# Hoja de trabajo 10 - 2 de junio del 2017
+
+
+
+
+
+# Importando neo4j
 from py2neo import Graph, authenticate, Node, Relationship
 authenticate("localhost:7474", "neo4j", "123456")
 graph = Graph()
@@ -5,9 +18,11 @@ graph = Graph()
 #graph.delete_all()
 loop = True
 
+# Mostrar el menu hasta que se desea salir
 while(loop):
     opcion = input("\nMenu:\n 1. Ingresar Doctor\n 2. Ingresar Paciente\n 3. Registrar Visita\n 4. Buscar doctores por especialidad\n 5. Registrar pacientes conocidos\n 6. Registrar doctores conocidos\n 7. Buscar doctor por conocidos\n 8. Salir\n")
 
+	# Para ingresar un doctor
     if(opcion == 1):
         sNombre = raw_input("Ingrese el nombre: ")
         sEspecialidad = raw_input("Ingrese la especialidad: ")
@@ -15,11 +30,15 @@ while(loop):
         sTelefono = raw_input("Ingrese el numero de telefono: ")
         doc = Node("Doctor", nombre=sNombre, especialidad=sEspecialidad, colegiado=sColegiado, telefono=sTelefono)
         graph.create(doc)
+	
+	# Para ingresar un paciente
     elif(opcion == 2):
         sNombre = raw_input("Ingrese el nombre: ")
         sTelefono = raw_input("Ingrese el numero de telefono: ")
         p = Node("Paciente", nombre=sNombre, telefono=sNombre)
         graph.create(p)
+	
+	# Para registrar una visita
     elif(opcion == 3):
         sNombre = raw_input("Ingrese el nombre del paciente: ")
         p = graph.find_one("Paciente", "nombre", sNombre)
@@ -38,11 +57,15 @@ while(loop):
         graph.create(visita)
         graph.create(toma)
         graph.create(prescribe)
+	
+	# Para buscar doctores por especialidad
     elif(opcion == 4):
         sEspecialidad = raw_input("Ingrese la especialidad: ")
         docs = graph.find("Doctor", "especialidad", sEspecialidad)
         for doc in docs:
             print(doc.properties["nombre"])
+	
+	# Para registrar pacientes conocidos
     elif(opcion == 5):
         sNombre = raw_input("Ingrese el numero del primer paciente: ")
         p = graph.find_one("Paciente", "nombre", sNombre)
@@ -50,6 +73,8 @@ while(loop):
         p2 = graph.find_one("Paciente", "nombre", sNombre)
         conoce = Relationship(p, "KNOWS", p2)
         graph.create(conoce)
+		
+	# Para registrar doctores conocidos
     elif(opcion == 6):
         sNombre = raw_input("Ingrese el numero del primer doctor: ")
         doc = graph.find_one("Doctor", "nombre", sNombre)
@@ -57,6 +82,8 @@ while(loop):
         doc2 = graph.find_one("Doctor", "nombre", sNombre)
         conoce = Relationship(doc, "KNOWS", doc2)
         graph.create(conoce)
+	
+	# Para buscar doctor por conocidos
     elif(opcion == 7):
         sNombre = raw_input("Ingrese el nombre del paciente que realizara la busqueda: ")
         p = graph.find_one("Paciente", "nombre", sNombre)
@@ -75,5 +102,7 @@ while(loop):
         #        for doc2 in docs2:
         #            if(doc2.properties("especialidad") == sEspecialidad):
         #                print(doc2.properties("nombre"))
+	
+	# Para salir del programa
     elif(opcion == 8):
         loop = False
